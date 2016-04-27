85c25fd0ab15df0c9504ef02b24162a569988f9f
Igor <OldPal@users.noreply.github.com>
2016-04-27 17:16:37 +0300

    Added files via upload

---
+++ 1.-LOOPS-ARRAYS/FirstOddNumber.java
@@ -0,0 +1,12 @@
+public class FirstOddNumber {
+
+    public int find(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 != 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
